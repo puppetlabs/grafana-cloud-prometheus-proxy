02ae5508194b765dbc606dea597cb60a59ae4b33
Mikker Gimenez-Peterson <mikker+05250246@puppet.com>
2020-04-03 14:04:21 -0700

    Fix the docker container to point to the right file

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Running Go Build"
-go get; cd cmd/web; go get; cd ../..
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o web cmd/web/main.go
+go get
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o web main.go
 
 TIMESTAMP=`date +%s`
 docker build --no-cache . -f Dockerfile -t $1:$2
